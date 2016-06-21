34f7159baa8ce772f009e3e74a0aaa275fa1ad9d
SemanticBeeng <nick@semanticbeeng.com>
2016-06-21 06:21:01 -0700

    fix

--- run.sh
+++ run.sh
@@ -2,9 +2,10 @@ cwd=/development/projects/01_devops/docker/ansible-inside-docker-demo/config
 
 docker run -it \
     -v /vagrant/hosts:$cwd/hosts \
-    -v /vagrant/insecure_private_key:$cwd/ansible_id_rsa \
+    -v /vagrant/insecure_private_key:$cwd/insecure_private_key \
     synergo/ansible-examples:0.1 \
     /sbin/my_init --skip-startup-files --skip-runit -- \
     ansible-playbook /srv/ansible/site.yml \
-    -i $cwd/hosts -u vagrant --private-key=$cwd/ansible_id_rsa
+    -i $cwd/hosts -u vagrant --private-key=$cwd/insecure_private_key
+
 
