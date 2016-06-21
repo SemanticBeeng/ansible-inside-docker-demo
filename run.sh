4cf208c51cc6d0ceabcfadfcb045371a9dcba824
SemanticBeeng <nick@semanticbeeng.com>
2016-06-21 04:31:48 -0700

    small cleanup

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-cwd=/development/projects/01_devops/docker/ansible-inside-docker-demo
+cwd=/development/projects/01_devops/docker/ansible-inside-docker-demo/config
 
 docker run -it \
     -v /vagrant/hosts:$cwd/hosts \
     -v /vagrant/insecure_private_key:$cwd/ansible_id_rsa \
-    ansible-examples \
+    synergo/ansible-examples:0.1 \
     /sbin/my_init --skip-startup-files --skip-runit -- \
     ansible-playbook /srv/ansible/site.yml \
     -i $cwd/hosts -u vagrant --private-key=$cwd/ansible_id_rsa
