3a5129fe3513240c475a554be7c769f12b062bac
SemanticBeeng <nick@semanticbeeng.com>
2016-06-21 07:02:34 -0700

    cleanup

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
-cwd=/development/projects/01_devops/docker/ansible-inside-docker-demo/config
+#cwd=/development/projects/01_devops/docker/ansible-inside-docker-demo/config
 
 docker run -it \
-    -v /vagrant/hosts:$cwd/hosts \
-    -v /vagrant/insecure_private_key:$cwd/insecure_private_key \
+    -v /vagrant/hosts:$(pwd)/hosts \
+    -v /vagrant/insecure_private_key:$(pwd)/insecure_private_key \
     synergo/ansible-examples:0.1 \
     /sbin/my_init --skip-startup-files --skip-runit -- \
-    ansible-playbook /srv/ansible/site.yml \
-    -i $cwd/hosts -u vagrant --private-key=$cwd/insecure_private_key
+    bash
+#    ansible-playbook /srv/ansible/site.yml \
+#    -i $(pwd)/hosts -u vagrant --private-key=$(pwd)/insecure_private_key
 
 
