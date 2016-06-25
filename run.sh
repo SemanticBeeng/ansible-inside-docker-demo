991c57550c77cabe13406fa6f88a230460f6c784
SemanticBeeng <nick@semanticbeeng.com>
2016-06-24 23:48:13 -0700

    fix attempt: errors in ansible-container build

--- run.sh
+++ run.sh
@@ -13,8 +13,10 @@ docker run -it \
     -v $cwd/ansible_id_rsa.pub:/root/.ssh/id_rsa.pub \
     -v $cprj/ansible:$mnt/ansible \
     -v $docker_projects:$mnt/ansible/projects \
+    -v /data:/data \
     -w $mnt/ansible \
     -v /usr/bin/docker:/usr/bin/docker \
+    -v /var/lib/docker:/var/lib/docker \
     -e DOCKER_HOST=$DOCKER_HOST \
     synergo/ansible-examples:0.1 \
     /sbin/my_init --skip-startup-files --skip-runit -- \
